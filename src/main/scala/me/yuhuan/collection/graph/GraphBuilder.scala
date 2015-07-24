f4b0378fad424826816a5fac001d237a0bbab2b0
yuhuan <jyuhuan@gmail.com>
2015-07-23 22:43:39 -0400

    + graph builder and factory

--- src/main/scala/me/yuhuan/collection/graph/GraphBuilder.scala
+++ src/main/scala/me/yuhuan/collection/graph/GraphBuilder.scala
@@ -1,17 +1,48 @@
 package me.yuhuan.collection.graph
 
 /**
+ * A Builder for any graph.
+ *
+ * @tparam I The type of the index of an vertex.
+ * @tparam V The type of the data in an vertex.
+ * @tparam E The type of the data of an edge.
+ *
  * @author Yuhuan Jiang (jyuhuan@gmail.com).
  */
 trait GraphBuilder[I, V, E] {
+  /**
+   * Adds a vertex to the graph.
+   * @param i The index of the vertex.
+   * @param v The data of the vertex.
+   */
   def addVertex(i: I, v: V)
+
+  /**
+   * Adds an edge to the graph.
+   * @param i The first index of the edge.
+   * @param j The second index of the edge.
+   * @param e The data of the edge.
+   */
   def addEdge(i: I, j: I, e: E)
 
   def +=(v: (I, V)) = addVertex(v._1, v._2)
   def +=(e: (I, I, E)) = addEdge(e._1, e._2, e._3)
 
+  /**
+   * Adds multiple vertices at once to the graph.
+   * @param vs The pairs of each vertex's index and data.
+   */
   def addVertices(vs: (I, V)*) = vs foreach +=
+
+  /**
+   * Adds multiple edges at once to the graph.
+   * @param es The triple of each edge's first and second indices and data.
+   */
   def addEdges(es: (I, I, E)*) = es foreach +=
 
+  /**
+   * Returns the graph built from this builder.
+   * @return A graph containing the vertices and edges added (if any).
+   */
   def result: Graph[I, V, E]
 }
--- src/main/scala/me/yuhuan/collection/graph/GraphFactory.scala
+++ src/main/scala/me/yuhuan/collection/graph/GraphFactory.scala
@@ -1,15 +1,53 @@
 package me.yuhuan.collection.graph
 
 /**
+ * Contains common constructors for any graph.
+ *
  * @author Yuhuan Jiang (jyuhuan@gmail.com).
+ * @tparam G The kind of the graph.
  */
 trait GraphFactory[G[_, _, _]] {
+  /**
+   * Returns a new builder for this kind of graph.
+   *
+   * @tparam I The type of the index of an vertex.
+   * @tparam V The type of the data in an vertex.
+   * @tparam E The type of the data of an edge.
+   *
+   * @return A new builder for this kind of graph.
+   */
   def newBuilder[I, V, E]: GraphBuilder[I, V, E]
 
+  /**
+   * Creates a graph by specifying the vertices and edges.
+   *
+   * @param vertices The vertices of the graph.
+   * @param edges The edges of the graph.
+   * @tparam I The type of the index of an vertex.
+   * @tparam V The type of the data in an vertex.
+   * @tparam E The type of the data of an edge.
+   *
+   * @return The graph which contains the specified vertices and edges.
+   */
   def apply[I, V, E](vertices: (I, V)*)(edges: (I, I, E)*): Graph[I, V, E] = {
     val b = newBuilder[I, V, E]
     b.addVertices(vertices: _*)
     b.addEdges(edges: _*)
     b.result
   }
+
+  /**
+   * Creates a graph by specifying only the data of the vertices and edges.
+   * IMPORTANT: The data of the vertices should be unique.
+   *            The data of the edges should also be unique.
+   *
+   * The indices are automatically managed.
+   *
+   * @param vertices The vertices of the graph.
+   * @param edges The edges of the graph.
+   * @tparam V The type of the data in an vertex.
+   * @tparam E The type of the data of an edge.
+   * @return
+   */
+  def apply[V, E](vertices: Set[V])(edges: (V, V, E)*): Graph[Int, V, E] = ???
 }
