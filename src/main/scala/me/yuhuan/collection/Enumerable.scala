026d4ad0360cbcd53cd090576b5b010c21445f48
yuhuan <jyuhuan@gmail.com>
2015-08-28 16:38:33 -0400

    + high order funcs

--- src/main/scala/me/yuhuan/collection/Enumerable.scala
+++ src/main/scala/me/yuhuan/collection/Enumerable.scala
@@ -9,7 +9,7 @@ trait Enumerable[+X] extends HasForeach[X] { outer ⇒
 
   def foreach[Y](f: (X ⇒ Y)): Unit = {
     val e = newEnumerator
-    while (e.advance()) {
+    while (e.moveNext()) {
       f(e.current)
     }
   }
@@ -28,8 +28,36 @@ trait Enumerable[+X] extends HasForeach[X] { outer ⇒
 
   def whereNot(p: X ⇒ Boolean) = where(a => !p(a))
 
+  def prepend[Y >: X](y: Y): Enumerable[Y] = new Enumerable[Y] {
+    def newEnumerator: Enumerator[Y] = outer.newEnumerator.prepend(y)
+  }
+
   def append[Y >: X](y: Y): Enumerable[Y] = new Enumerable[Y] {
     def newEnumerator: Enumerator[Y] = outer.newEnumerator.append(y)
   }
 
+  def take(n: Int): Enumerable[X] = new Enumerable[X] {
+    def newEnumerator: Enumerator[X] = outer.newEnumerator.take(n)
+  }
+
+  def skip(n: Int): Enumerable[X] = new Enumerable[X] {
+    def newEnumerator: Enumerator[X] = outer.newEnumerator.skip(n)
+  }
+
+  def slice(start: Int, end: Int): Enumerable[X] = new Enumerable[X] {
+    def newEnumerator: Enumerator[X] = outer.newEnumerator.slice(start, end)
+  }
+
+  def window(size: Int): Enumerable[X] = new Enumerable[X] {
+    def newEnumerator: Enumerator[X] = ???
+  }
+
+  def str: String = {
+    val sb = new StringBuilder()
+    outer.foreach(x ⇒ {
+      sb append x
+      sb append ", "
+    })
+    sb.toString()
+  }
 }
--- src/main/scala/me/yuhuan/collection/Enumerator.scala
+++ src/main/scala/me/yuhuan/collection/Enumerator.scala
@@ -1,26 +1,30 @@
 package me.yuhuan.collection
 
+import me.yuhuan.collection.basicimp.ResizableArray
+
 /**
  * @author Yuhuan Jiang (jyuhuan@gmail.com).
  */
 trait Enumerator[+X] { outer ⇒
-  def advance(): Boolean
+  def moveNext(): Boolean
   def current: X
 
   def map[Y](f: X ⇒ Y): Enumerator[Y] = new Enumerator[Y] {
-    def advance(): Boolean = outer.advance()
+    def moveNext(): Boolean = outer.moveNext()
     def current: Y = f(outer.current)
   }
 
+  def fmap[Y](f: X ⇒ Enumerator[Y]) = flatMap(f)
+
   def flatMap[Y](f: X ⇒ Enumerator[Y]) = new Enumerator[Y] { inner ⇒
     var curEnumerator: Enumerator[Y] = Enumerator.empty
-    def advance(): Boolean = {
-      if (curEnumerator.advance()) true
+    def moveNext(): Boolean = {
+      if (curEnumerator.moveNext()) true
       else {
-        if (outer.advance()) {
+        if (outer.moveNext()) {
           curEnumerator = f(outer.current)
-          if (curEnumerator.advance()) true
-          else inner.advance()
+          if (curEnumerator.moveNext()) true
+          else inner.moveNext()
         }
         else false
       }
@@ -29,8 +33,8 @@ trait Enumerator[+X] { outer ⇒
   }
 
   def where(p: X ⇒ Boolean): Enumerator[X] = new Enumerator[X] {   // Y N N Y Y N N Y N
-    def advance(): Boolean = {
-      while (outer.advance()) if (p(outer.current)) return true
+    def moveNext(): Boolean = {
+      while (outer.moveNext()) if (p(outer.current)) return true
       false
     }
     def current: X = outer.current
@@ -38,18 +42,35 @@ trait Enumerator[+X] { outer ⇒
 
   def whereNot(p: X ⇒ Boolean): Enumerator[X] = where(a => !p(a))
 
+  def prepend[Y >: X](y: Y): Enumerator[Y] = new Enumerator[Y] {
+    var isInOriginalRange = false
+    var cur: Y = _
+    def moveNext(): Boolean = {
+      if (!isInOriginalRange) {
+        cur = y
+        isInOriginalRange = true
+        true
+      }
+      else {
+        val outerHashNext = outer.moveNext()
+        if (outerHashNext) cur = outer.current
+        outerHashNext
+      }
+
+    }
+    def current: Y = cur
+  }
+
   def append[Y >: X](y: Y): Enumerator[Y] = new Enumerator[Y] {
     var isInOriginalRange = true
-    def advance(): Boolean = {
-      if (outer.advance()) {
-        return true
-      }
+    def moveNext(): Boolean = {
+      if (outer.moveNext()) true
       else {
         if (isInOriginalRange) {
           isInOriginalRange = false
-          return true
+          true
         }
-        else return false
+        else false
       }
     }
     def current: Y = {
@@ -58,11 +79,48 @@ trait Enumerator[+X] { outer ⇒
     }
   }
 
+  def take(n: Int): Enumerator[X] = new Enumerator[X] {
+    var curIdx = 0
+    def moveNext(): Boolean = {
+      if (outer.moveNext() && curIdx < n) {
+        curIdx += 1
+        true
+      }
+      else false
+    }
+    def current: X = outer.current
+  }
+
+  def skip(n: Int): Enumerator[X] = new Enumerator[X] {
+
+    var isFirstMove = true
+
+    def moveNext(): Boolean = {
+      if (isFirstMove) {
+        isFirstMove = false
+        var moveCount = n
+        while (moveCount >= 0) {
+          outer.moveNext()
+          moveCount -= 1
+        }
+        true
+      }
+      else outer.moveNext()
+    }
+
+    def current: X = outer.current
+  }
+
+  def slice(start: Int, end: Int): Enumerator[X] = outer.skip(start).take(end - 1)
+
+  def window(size: Int): Enumerator[X] = ???
+
+
 }
 
 object Enumerator {
   def empty: Enumerator[Nothing] = new Enumerator[Nothing] {
-    def advance(): Boolean = false
+    def moveNext(): Boolean = false
     def current: Nothing = throw new NoSuchElementException()
   }
 }
