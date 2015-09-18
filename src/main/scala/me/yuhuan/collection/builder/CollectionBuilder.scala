8654271a3a4a8b79620f13afa1ac2628f298fa0e
yuhuan <jyuhuan@gmail.com>
2015-09-18 18:09:57 -0400

    relaxed trait requirement

--- src/main/scala/me/yuhuan/collection/builder/CollectionBuilder.scala
+++ src/main/scala/me/yuhuan/collection/builder/CollectionBuilder.scala
@@ -1,13 +1,15 @@
 package me.yuhuan.collection.builder
 
-import me.yuhuan.collection.Enumerable
+import me.yuhuan.collection.HasForeach
 
 /**
  * @author Yuhuan Jiang (jyuhuan@gmail.com).
  */
 trait CollectionBuilder[-T, +C] {
   def add(x: T): Unit
-  def addMany(xs: Enumerable[T]) = xs.foreach(add)
+  def addMany(xs: HasForeach[T]) = xs.foreach(add)
   def result: C
   def sizeHint: Int
+  def +(x: T): Unit
+  def ++(xs: HasForeach[T]) = xs.foreach(add)
 }
